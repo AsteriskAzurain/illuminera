90d7fa494195215787c6d3443fa094317ed67fdc
Asterisk Azurain <qysx32_kitty@yeah.net>
2021-03-31 13:01:35 +0800

    修复了循环中, soup读取内容不刷新的bug
    添加driver.refresh()获取新页面内容

--- checkEnable.py
+++ checkEnable.py
@@ -7,10 +7,12 @@ from datetime import datetime
 
 from bs4 import BeautifulSoup
 import pymssql
+import requests
 
 options = Options()
 options.add_argument('--headless')
 driver = webdriver.Chrome(options=options)
+# driver = webdriver.Chrome()
 driver.get('http://192.168.1.245:8088/login.aspx')
 # sleep(3)
 WebDriverWait(driver, 10)
@@ -23,40 +25,46 @@ driver.find_element_by_id('txtPassword').send_keys(password)
 driver.find_element_by_id('btnlogin').click()
 sleep(3)
 
-server="192.168.1.245"
-user="sa"
-password="Illuminera2011"
-database="IllumineraERP"
+server = "192.168.1.245"
+user = "sa"
+password = "Illuminera2011"
+database = "IllumineraERP"
 conn = pymssql.connect(server, user, password, database)
 
 cursor = conn.cursor()
-sql = "select sqlFileID from [t_Jobs_P2FConvertRecord] where createTime > '{0}'"
-sql=sql.format(datetime.now().strftime("%Y-%m-%d"))
+sql = "select sqlFileID from [t_Jobs_P2FConvertRecord] where createTime > '{0}' "
+sql = sql.format(datetime.now().strftime("%Y-%m-%d"))
 
 cursor.execute(sql)
-ls_fileID=cursor.fetchall()
+ls_fileID = cursor.fetchall()
 conn.close()
 
 docUrl = "http://192.168.1.245:8088/ARES/SWF_FILE/docviewer.html#{0}.xml"
 
-ls_fail=[]
+ls_fail = []
 for fileid in ls_fileID:
-    fileid=fileid[0]
-    url=docUrl.format(fileid)
+    fileid = fileid[0]
+    url = docUrl.format(fileid)
     driver.get(url)
+    driver.refresh()
+    sleep(5)
     html = driver.page_source
-    soup = BeautifulSoup(html,features="html.parser")
+    soup = BeautifulSoup(html, features="html.parser")
 
+    textmsg = ""
+    pagelist = []
     textmsg = soup.find('span', id='textmsg').get_text()
     pagelist = soup.find_all('div', attrs={"class": "pagecont"})
 
-    print(fileid,'\t',"Success" if len(pagelist) > 0 else textmsg)
-    
-    if(len(pagelist)==0): ls_fail.append(fileid)
+    print(fileid, '\t', "Success" if len(pagelist) > 0 else textmsg)
+
+    if(len(pagelist) == 0):
+        ls_fail.append(fileid)
 
 driver.close()
 
-print('\n\n失败个数:',len(ls_fail))
+print('\n\n失败个数:', len(ls_fail))
 
-if (len(ls_fail)): 
-    for fail_id in ls_fail: print(fail_id)
+if (len(ls_fail)):
+    for fail_id in ls_fail:
+        print(fail_id)
